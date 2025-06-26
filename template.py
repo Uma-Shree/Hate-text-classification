833144c81b729f10ed8d8398e94407db2c5fa797
Uma Shree <umashree31jan@gmail.com>
2025-06-26 18:38:59 +0530

    required files created

--- template.py
+++ template.py
@@ -13,6 +13,7 @@ list_of_files= [
     f"{project_name}/components/model_trainer.py",
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
     f"{project_name}/configuration/gcloud_syncer.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/config_entity.py",
